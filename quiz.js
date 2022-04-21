c90333229ac3e15525dff147544be0964707540b
suzy-thetraveler <suzythetraveler@khu.ac.kr>
2022-04-21 23:24:12 +0900

    class 문제 오답노트

--- quiz.js
+++ quiz.js
@@ -2,13 +2,16 @@
   /* quiz 1 */
 }
 class Counter {
+  //private field값을 만들어서 외부에서 접근하지 못하면서 쉽게 값을 찾을 수 있도록 해야함
   constructor(count) {
-    this.count = count;
+    this.count = count; //여기서 숫자값이 아닐때, 혹은 음수일 때에 대한 부분을 체크해야함
   }
   countNum() {
+    //메소드명에 대한 부분 고민하기, 증가!이면 increment가 훨씬 자연스러움
     this.count++;
     console.log(this.count);
   }
+  //값을 확인 할 수 있는 get 메서드를 만들어주면 좋음
 }
 
 const counter = new Counter(4);
